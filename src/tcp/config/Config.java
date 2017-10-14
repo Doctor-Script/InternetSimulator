768141ce1bff5cc0e3c4ed2b79f86467c12fd611
Doctor-Script <serhyy.kovalenko@i.ua>
2017-10-14 21:54:19 +0300

    +1

--- src/tcp/config/Config.java
+++ src/tcp/config/Config.java
@@ -9,9 +9,9 @@ import java.util.logging.SimpleFormatter;
 public class Config
 {
 	public static final String SERVER_ADDRESS = "127.0.0.1";
-	public static final int SERVER_PORT = 8080;
+	public static final int SERVER_PORT = 54811;
 	public static final long PING = 2000;
-	public static final int LISTEN_PORT = 8081;
+	public static final int LISTEN_PORT = 54812;
 	public static final String LOG_FILE = "AllLog.log";
 	
 	private static Logger logger = null;
